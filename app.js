85702a909ca7a79711e11566c69b1053b04a571e
Taimoor Ali <taimoor736@gmail.com>
2021-10-10 18:13:11 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+/*
+
+Statement:
+Wrtie a function that takes a nested object and return the flattened version of it.
+
+Example:
+const nestedObject1 = {
+	a: {
+		foo: 'foo',
+		bar: {
+			bat: 1,
+			ball: 'ball',
+		}
+	},
+	b: 'test',
+	c: {
+		name: 'test'
+	}
+}
+
+flattenObject(nestedObject1)
+should return {
+	foo: 'foo',
+	bat: 1,
+	ball: 'ball',
+	b: 'test',
+	name: 'test'
+}
+
+*/
+
+export function flattenObject() {
+	// code here... Hello There
+}
---
+++ app.test.js
@@ -0,0 +1,10 @@
+const { flattenObject } = require('./app');
+
+describe('app.js', () => {
+	describe('flattenObject()', () => {
+		it('should have tests', () => {
+			expect(false).toBeTruthy();
+		})
+	});
+
+})
